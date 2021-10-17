e94e6898fe8c5dda2c9123fa92265fe840a7f05e
234749 Maciej Adamczewski <234749@edu.p.lodz.pl>
2021-10-17 23:20:57 +0200

    CMake Configuration 2

---
+++ labs/project/program/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Student.h"
+int main ()
+{
+std::cout <<"Hello, OOP1!"<<std::endl;
+Student student("Thomas", "Edison");
+std::cout<<"Student"<<student.firstName<<" "<<student.lastName<<std::endl;
+return 0;
+}
