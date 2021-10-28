84779dd6ca8043ffa1dca0130c46981383e7a322
234749 Maciej Adamczewski <234749@edu.p.lodz.pl>
2021-10-28 16:19:08 +0200

    Basic unit testing implementation

---
+++ labs/project/library/test/ContainerTest.cpp
@@ -0,0 +1,46 @@
+#include <boost/test/unit_test.hpp>
+
+#include "Container.h"
+
+BOOST_AUTO_TEST_SUITE(TestContainerConstructorWithNumber)
+
+    BOOST_AUTO_TEST_CASE(ConstructorWithNumber) {
+        Container container(1);
+        BOOST_REQUIRE_EQUAL(container.getNumber(), 1);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE(TestContainerConstructorWithout)
+
+        BOOST_AUTO_TEST_CASE(ConstructorWithout) {
+        Container container();
+        BOOST_REQUIRE_EQUAL(container.getNumber(), 0);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE(TestEmptyContainer)
+
+        BOOST_AUTO_TEST_CASE(ContainerGrossWeight) {
+        Container container();
+        BOOST_REQUIRE_EQUAL(container.getGrossWeight(), container.getTareWeight());
+    }
+        BOOST_AUTO_TEST_CASE(ContainerNetWeight) {
+        Container container();
+        BOOST_REQUIRE_EQUAL(container.getNetWeight(), 0);
+    }
+        BOOST_AUTO_TEST_CASE(ContainerMaxWeight) {
+        Container container();
+        double result = container.getMaxWeight() - container.getTareWeight();
+        BOOST_REQUIRE_EQUAL(container.getMaxCargo(), result);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE(TestLoadCargoBelowMax)
+
+        BOOST_AUTO_TEST_CASE(ContainerLoad) {
+        Container container();
+        container.loadCargo(420.69)
+        BOOST_REQUIRE_EQUAL(container.getNetWeight(), 420.69);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
