d0fe3de384c5a96550e8182b6f37849e856f5862
234749 Maciej Adamczewski <234749@edu.p.lodz.pl>
2021-11-08 12:51:48 +0100

    Unit test naming convention applied.

--- labs/project/library/test/ContainerTest.cpp
+++ labs/project/library/test/ContainerTest.cpp
@@ -2,42 +2,27 @@
 
 #include "Container.h"
 
-BOOST_AUTO_TEST_SUITE(TestContainerConstructorWithNumber)
+BOOST_AUTO_TEST_SUITE(TestContainerSuite)
 
-    BOOST_AUTO_TEST_CASE(ConstructorWithNumber) {
+    BOOST_AUTO_TEST_CASE(HasNumberWhenWithParameter) {
         Container container(1);
         BOOST_REQUIRE_EQUAL(container.getNumber(), 1);
     }
 
-BOOST_AUTO_TEST_SUITE_END()
-BOOST_AUTO_TEST_SUITE(TestContainerConstructorWithout)
-
-        BOOST_AUTO_TEST_CASE(ConstructorWithout) {
+        BOOST_AUTO_TEST_CASE(HasNumber0WhenNoParameters) {
         Container container;
         BOOST_REQUIRE_EQUAL(container.getNumber(), 0);
     }
 
-BOOST_AUTO_TEST_SUITE_END()
-BOOST_AUTO_TEST_SUITE(TestEmptyContainer)
-
-        BOOST_AUTO_TEST_CASE(ContainerGrossWeight) {
+        BOOST_AUTO_TEST_CASE(EmptyWhenNoInputs) {
         Container container;
         BOOST_REQUIRE_EQUAL(container.getGrossWeight(), container.getTare());
-    }
-        BOOST_AUTO_TEST_CASE(ContainerNetWeight) {
-        Container container;
         BOOST_REQUIRE_EQUAL(container.getNetWeight(), 0);
-    }
-        BOOST_AUTO_TEST_CASE(ContainerMaxWeight) {
-        Container container;
         double result = container.getMaxWeight() - container.getTare();
         BOOST_REQUIRE_EQUAL(container.getMaxCargo(), result);
-    }
+        }
 
-BOOST_AUTO_TEST_SUITE_END()
-BOOST_AUTO_TEST_SUITE(TestLoadCargoBelowMax)
-
-        BOOST_AUTO_TEST_CASE(ContainerLoad) {
+        BOOST_AUTO_TEST_CASE(ContainsCargoWhenLoaded) {
         Container container;
         container.setMaxWeight(500.0);
         container.loadCargo(420.69);
