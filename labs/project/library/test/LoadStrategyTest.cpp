7f517433eafe0148f82fd3fd6a6af0469dca80b5
234749 Maciej Adamczewski <234749@edu.p.lodz.pl>
2021-12-06 13:53:21 +0100

    LoadStrategy unit test.

---
+++ labs/project/library/test/LoadStrategyTest.cpp
@@ -0,0 +1,12 @@
+#include <boost/test/unit_test.hpp>
+#include "Strategies/LoadStrategy.h"
+
+BOOST_AUTO_TEST_SUITE(TestLoadStrategySuite)
+
+    BOOST_AUTO_TEST_CASE(IsInitiallyEmpty) {
+
+        std::cout << "lol";
+        BOOST_REQUIRE_EQUAL(0, 0);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
